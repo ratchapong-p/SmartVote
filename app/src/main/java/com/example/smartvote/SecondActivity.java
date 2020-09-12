991ddcee1082a65b8fb1819ae43e9b81019e3751
Ratchapong Kongbua <theoutlaw001@hotmail.com>
2020-09-12 12:22:38 +0700

    homework android #2

---
+++ app/src/main/java/com/example/smartvote/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.example.smartvote;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+        Button checkButton = findViewById(R.id.check_button);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView IdNumber = findViewById(R.id.TextNumber);
+                String numText = IdNumber.getText().toString();
+
+                if (numText.length() == 13) {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(SecondActivity.this);
+                    dialog.setTitle("ผลการตรวจสอบสิทธิเลือกตั้ง");
+                    dialog.setPositiveButton("OK", null);
+                    if (numText.equals("1111111111111") || numText.equals("2222222222222")) {
+                        dialog.setMessage("คุณเป็นผู้มีสิทธิเลือกตั้ง");
+                    } else {
+                        dialog.setMessage("คุณไม่มีสิทธิเลือกตั้ง");
+                    }
+
+                    dialog.show();
+
+                } else {
+                    Toast t = Toast.makeText(SecondActivity.this,"กรุณากรอกหมายเลขบัตรประชาชน 13 หลัก", Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
+
+    }
+}
